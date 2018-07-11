23299d7a4b784c428e0458ce2dd462060865283e
ghsmith <geoffrey.hughes.smith@gmail.com>
2018-07-11 10:19:59 -0400

    Now accomodate frequency with "<" sign (by ignoring the "<" sign).

--- src/main/java/edu/emory/mrp/MyeloidReportParser.java
+++ src/main/java/edu/emory/mrp/MyeloidReportParser.java
@@ -107,7 +107,7 @@ public class MyeloidReportParser {
                         else if(line.startsWith("II. Tier 2"))                           { variantCategory = "Tier 2: Unknown significance"; }
                         else if(line.startsWith("III. Single Nucleotide Polymorphisms")) { variantCategory = "SNP with significance"; }
                         {
-                            Pattern pattern = Pattern.compile("^[0-9]+\\. ([^ ]+) (c\\.[^ ]+), (p\\.[^ ]+) \\((NM_[0-9\\.]+)\\) Variant Frequency: ([0-9\\.]+)%");
+                            Pattern pattern = Pattern.compile("^[0-9]+\\. ([^ ]+) (c\\.[^ ]+), (p\\.[^ ]+) \\((NM_[0-9\\.]+)\\) Variant Frequency: <?([0-9\\.]+)%");
                             Matcher matcher = pattern.matcher(line);
                             if(matcher.matches()) {
                                 variant = new Variant();
@@ -121,7 +121,7 @@ public class MyeloidReportParser {
                             }
                         }
                         {
-                            Pattern pattern = Pattern.compile("^[0-9]+\\. ([^ ]+) (c\\.[^ ]+) \\((NM_[0-9\\.]+)\\) Variant Frequency: ([0-9\\.]+)%");
+                            Pattern pattern = Pattern.compile("^[0-9]+\\. ([^ ]+) (c\\.[^ ]+) \\((NM_[0-9\\.]+)\\) Variant Frequency: <?([0-9\\.]+)%");
                             Matcher matcher = pattern.matcher(line);
                             if(matcher.matches()) {
                                 variant = new Variant();
